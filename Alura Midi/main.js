80106337520b6db1004e30ef8960316180feaf58
Bruno Souza <brunosouza.cup@gmail.com>
2023-04-12 15:39:05 -0300

    code update Alura Midi

--- Alura Midi/main.js
+++ Alura Midi/main.js
@@ -3,10 +3,7 @@ function playSong(idElementSong){
     document.querySelector(idElementSong).play();
 }
 
-// referencia constante 
 const KeyList = document.querySelectorAll('.tecla');
-// referencia variavel
-
 
 for(let accountant = 0; accountant < KeyList.length; accountant++){
 
