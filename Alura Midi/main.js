6c00b0b386bb2f6fa6fd3fdb9b81a8cf3b6bc78a
Bruno Souza <brunosouza.cup@gmail.com>
2023-04-12 16:06:17 -0300

    keyEvent update Alura Midi

--- Alura Midi/main.js
+++ Alura Midi/main.js
@@ -1,27 +1,46 @@
-/*
+/*  --------------------------------------
     Implementação da reprodução dos Sons
-*/
-function playSong(idElementSong){
+    -------------------------------------- */
+function playSong(idElementSong) {
 
     document.querySelector(idElementSong).play();
 }
 
 const KeyList = document.querySelectorAll('.tecla');
 
-for(let accountant = 0; accountant < KeyList.length; accountant++){
+for (let accountant = 0; accountant < KeyList.length; accountant++) {
 
     const key = KeyList[accountant];
     const instrument = key.classList[1];
     const idAudio = `#som_${instrument}`; //template string
-    
-    KeyList[accountant].onclick = function(){
+
+    KeyList[accountant].onclick = function () {
         playSong(idAudio);
     }
 
     //console.log(idAudio);
     //console.log(accountant);
-}
 
-/*
-    Eventos de Teclado
-*/
+    /*  -------------------
+        Eventos de Teclado
+        ------------------- */
+
+    key.onkeydown = function (event) {
+
+        //console.log(event.code);
+
+        if (event.code === 'Espace' || event.code === 'Enter') {
+            key.classList.add('ativa');
+        }
+
+        if (event.code === 'Enter') {
+            key.classList.add('ativa');
+        }
+
+    }
+
+    key.onkeyup = function () {
+        key.classList.remove('ativa');
+
+    }
+}
